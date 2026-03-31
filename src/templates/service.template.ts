e2ad5f40df52bc646244b3d0ed174d012e0b7a7d
Chirag Chauhan <chiragvchauhan93@gmail.com>
2026-03-31 14:41:00 +0530

    Capitalize name

--- src/templates/service.template.ts
+++ src/templates/service.template.ts
@@ -10,13 +10,13 @@ const serviceGenrate = async (
   isCrud: boolean,
 ) => {
   let serviceContent = "";
-
+  const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
   if (selectedDb === "mongodb") {
     const populateFields = relations?.map((r: any) => r.field).filter(Boolean);
     const uniqueTargets = [...new Set(relations.map((r: any) => r.target))];
 
     const relationImportsESM = uniqueTargets
-      .map((t) => `import ${t} from "../models/${t}.model.js";`)
+      .map((t) => `import  "../models/${t}.model.js";`)
       .join("\n");
 
     const relationImportsCJS = uniqueTargets
@@ -25,57 +25,57 @@ const serviceGenrate = async (
 
     serviceContent = isESM
       ? `${relationImportsESM}
-import ${name} from "../models/${name}.model.js";
+import ${nameCapitalized} from "../models/${nameCapitalized}.model.js";
 
 export const getAll = async () => {
-  return await ${name}.find().populate(${JSON.stringify(
+  return await ${nameCapitalized}.find().populate(${JSON.stringify(
     populateFields,
   )}.map(f => ({ path: f })));
 };
 
 export const getById = async (id) => {
-  return await ${name}.findById(id).populate(${JSON.stringify(
+  return await ${nameCapitalized}.findById(id).populate(${JSON.stringify(
     populateFields,
   )}.map(f => ({ path: f })));
 };
 
 export const create = async (data) => {
-  return await ${name}.create(data);
+  return await ${nameCapitalized}.create(data);
 };
 
 export const update = async (id, data) => {
-  return await ${name}.findByIdAndUpdate(id, data, { new: true });
+  return await ${nameCapitalized}.findByIdAndUpdate(id, data, { new: true });
 };
 
 export const remove = async (id) => {
-  return await ${name}.findByIdAndDelete(id);
+  return await ${nameCapitalized}.findByIdAndDelete(id);
 };
 `
-      : `const ${name} = require("../models/${name}.model");
+      : `const ${nameCapitalized} = require("../models/${nameCapitalized}.model");
 ${relationImportsCJS}
 
 module.exports.getAll = async () => {
-  return await ${name}.find().populate(${JSON.stringify(
+  return await ${nameCapitalized}.find().populate(${JSON.stringify(
     populateFields,
   )}.map(f => ({ path: f })));
 };
 
 module.exports.getById = async (id) => {
-  return await ${name}.findById(id).populate(${JSON.stringify(
+  return await ${nameCapitalized}.findById(id).populate(${JSON.stringify(
     populateFields,
   )}.map(f => ({ path: f })));
 };
 
 module.exports.create = async (data) => {
-  return await ${name}.create(data);
+  return await ${nameCapitalized}.create(data);
 };
 
 module.exports.update = async (id, data) => {
-  return await ${name}.findByIdAndUpdate(id, data, { new: true });
+  return await ${nameCapitalized}.findByIdAndUpdate(id, data, { new: true });
 };
 
 module.exports.remove = async (id) => {
-  return await ${name}.findByIdAndDelete(id);
+  return await ${nameCapitalized}.findByIdAndDelete(id);
 };
 `;
   } else {
@@ -84,65 +84,65 @@ module.exports.remove = async (id) => {
       .join(",");
 
     serviceContent = isESM
-      ? `import ${name} from "../models/${name}.model.js";
+      ? `import ${nameCapitalized} from "../models/${nameCapitalized}.model.js";
 import * as models from "../models/index.js";
 
 export const getAll = async () => {
-  return await ${name}.findAll({
+  return await ${nameCapitalized}.findAll({
     include: [${includeCode}]
   });
 };
 
 export const getById = async (id) => {
-  return await ${name}.findByPk(id, {
+  return await ${nameCapitalized}.findByPk(id, {
     include: [${includeCode}]
   });
 };
 
 export const create = async (data) => {
-  return await ${name}.create(data);
+  return await ${nameCapitalized}.create(data);
 };
 
 export const update = async (id, data) => {
-  const record = await ${name}.findByPk(id);
+  const record = await ${nameCapitalized}.findByPk(id);
   if (!record) return null;
   return await record.update(data);
 };
 
 export const remove = async (id) => {
-  const record = await ${name}.findByPk(id);
+  const record = await ${nameCapitalized}.findByPk(id);
   if (!record) return null;
   await record.destroy();
   return true;
 };
 `
-      : `const ${name} = require("../models/${name}.model");
+      : `const ${nameCapitalized} = require("../models/${nameCapitalized}.model");
 const models = require("../models");
 
 module.exports.getAll = async () => {
-  return await ${name}.findAll({
+  return await ${nameCapitalized}.findAll({
     include: [${includeCode}]
   });
 };
 
 module.exports.getById = async (id) => {
-  return await ${name}.findByPk(id, {
+  return await ${nameCapitalized}.findByPk(id, {
     include: [${includeCode}]
   });
 };
 
 module.exports.create = async (data) => {
-  return await ${name}.create(data);
+  return await ${nameCapitalized}.create(data);
 };
 
 module.exports.update = async (id, data) => {
-  const record = await ${name}.findByPk(id);
+  const record = await ${nameCapitalized}.findByPk(id);
   if (!record) return null;
   return await record.update(data);
 };
 
 module.exports.remove = async (id) => {
-  const record = await ${name}.findByPk(id);
+  const record = await ${nameCapitalized}.findByPk(id);
   if (!record) return null;
   await record.destroy();
   return true;
